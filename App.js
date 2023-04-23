db27b47e3e15cbad239845870a3f775626a07821
Aiden Chen <70079892+sorata000x@users.noreply.github.com>
2023-04-23 14:19:25 -0700

    Work out conflict between input-response and visual-function (#3)
    * Move style from index.html to styles.css
    
    * Move input-response function script from index.html to App.js
    
    * Remove placeholder background image
    
    * Work out conflict between input-response and visial function

--- App.js
+++ App.js
@@ -9,3 +9,35 @@ function changeImage(){
     let image = document.getElementById("8ball");
     image.src = listOfImages[random];
 }
+
+// Function to handle button clicks and display the 8 ball answer.
+function getAnswer() {
+    console.log('getAnswer');
+
+    var question = document.getElementById("question").value;
+    var answers = [
+      "It is certain.",
+      "It is decidedly so.",
+      "Without a doubt.",
+      "Yes - definitely.",
+      "You may rely on it.",
+      "As I see it, yes.",
+      "Most likely.",
+      "Outlook good.",
+      "Yes.",
+      "Signs point to yes.",
+      "Reply hazy, try again.",
+      "Ask again later.",
+      "Better not tell you now.",
+      "Cannot predict now.",
+      "Concentrate and ask again.",
+      "Don't count on it.",
+      "Outlook not so good.",
+      "My sources say no.",
+      "Very doubtful.",
+      "My reply is no."
+    ];
+    var answer = answers[Math.floor(Math.random() * answers.length)];
+    document.getElementById("question").value = "";
+    document.getElementById("answer").innerHTML = "<strong>Question:</strong> " + question + "<br><strong>Answer:</strong> " + answer;
+  }
