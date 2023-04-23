c14d2e73484edc46d871aad3bc0d3f1c8cd87bf5
chisengwong <aaabab239251@gmail.com>
2023-04-23 12:54:27 -0700

    modulized

--- App.js
+++ App.js
@@ -1,2 +1,11 @@
 //TODO: Shashank: fill the array with a bunch chatGPT gen responses
 const listOfTextToDisplay = ['temp']
+
+// Add JavaScript code here to handle button clicks and display the 8 ball answer.
+const listOfImages = ['./assests/8ball1.png', "./assests/8ball2.jpg"]
+function changeImage(){
+    console.log('changeImage() call');
+    let random = Math.floor(Math.random() * listOfImages.length);
+    let image = document.getElementById("8ball");
+    image.src = listOfImages[random];
+}
